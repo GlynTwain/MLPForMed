c700666ad4c973b7e52707ab2571c88adf7a7e67
GlynTwain <tri.dryga.new@gmail.com>
2020-03-17 00:40:33 +0300

    Внёс Метод, оставлю сегодня всё на проблеме с созданием файла, кажется я чего то не понимаю

--- Main.py
+++ Main.py
@@ -2,15 +2,18 @@ import json
 import requests
 import os
 
-folderName = "tasks"
-os.mkdir(folderName)
-if os.path.exists(folderName)
+folderName: str = "tasks"
+if not os.path.exists(folderName):
+    os.mkdir(folderName)
 
 todos_json = requests.get("https://json.medrating.org/todos")
 user_json = requests.get("https://json.medrating.org/users")
 
 myUserJson = json.loads(user_json.text)
+print(myUserJson[3]["name"])
 
 
-
-print(myUserJson[1]["name"])
+def napechate():
+    """Сформировать файл"""
+    outfilename = myUserJson[1]["name"] + ".txt"
+    file = open(myUserJson[1]["name"], mode='w+', encoding='utf-8')
