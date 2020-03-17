4aabbf98bb46c763c3dce38dc81a847134c6d421
GlynTwain <tri.dryga.new@gmail.com>
2020-03-17 21:25:19 +0300

    Ввожу новые методы для работы скрипта в Linux... Потом посмотрим что выстрелит

--- Main.py
+++ Main.py
@@ -15,32 +15,24 @@ user_json = requests.get("https://json.medrating.org/users")
 myUserJson = json.loads(user_json.text)
 
 
-def creation_date(path_to_file):
-    """
-    Try to get the date that a file was created, falling back to when it was
-    last modified if that isn't possible.
-    See http://stackoverflow.com/a/39501288/1709587 for explanation.
-    """
-    if platform.system() == 'Windows':
-        return os.path.getctime(path_to_file)
-    else:
-        stat = os.stat(path_to_file)
-        try:
-            return stat.st_birthtime
-        except AttributeError:
-            # We're probably on Linux. No easy way to get creation dates here,
-            # so we'll settle for when its content was last modified.
-            return stat.st_mtime
+def creation_date(name_file):
+    file_for_rename = open(name_file)
+    line = file_for_rename.readline()
+    line = line.partition("> ")[2].replace(".","-").replace(":","-")
+    line = line[0:16]
+
+    file_for_rename.close()
+    return line
 
 
 def modification_date_for_rename(filename):
     t = os.path.getmtime(filename)
-    return datetime.datetime.fromtimestamp(t).strftime("%d-%m-%YT %I-%M")
+    return datetime.datetime.fromtimestamp(t).strftime("%d-%m-%YT %H-%M")
 
 
 def modification_date(filename):
     t = os.path.getmtime(filename)
-    return datetime.datetime.fromtimestamp(t).strftime("%d.%m.%Y %I:%M")
+    return datetime.datetime.fromtimestamp(t).strftime("%d.%m.%Y %H:%M")
 
 
 def create_file(user):
@@ -48,7 +40,9 @@ def create_file(user):
     name_file = folderName + "/" + user["username"] + ".txt"
 
     if os.path.exists(name_file):
-        new_name = folderName + "/" + user["username"] + "_" + modification_date_for_rename(name_file) + ".txt"
+
+        new_name = folderName + "/" + user["username"] + "_" + creation_date(name_file) + ".txt"
+        creation_date(name_file)
         os.rename(name_file, new_name)
 
     if not os.path.exists(name_file):
@@ -59,34 +53,21 @@ def filling(user, name_file):
     file = open(name_file, mode='w+', encoding='utf-8')
     file.write(user["name"] + " <" + user["email"] + "> " + modification_date(name_file) + '\n')
     file.write(user["company"]["name"] + '\n')
-    file.write('\n'+"Завершенные задачи:" + '\n')
-    for todos_from_base in myTodosJson:
-        if user["id"] == todos_from_base["userId"] and todos_from_base["completed"] == True:
-            task = todos_from_base["title"]
-            if len(task) >= 51:
-                file.write(task[0: 50] + "..." + '\n')
-            else:
-                file.write(task + '\n')
-    file.write('\n'+"Оставшиеся задачи:" + '\n')
-    for todos_from_base in myTodosJson:
-        if user["id"] == todos_from_base["userId"] and todos_from_base["completed"] == False:
-            task = todos_from_base["title"]
-            if len(task) >= 51:
-                file.write(task[0: 50] + "..." + '\n')
-            else:
-                file.write(task + '\n')
+    work_todos(user["id"], file, "Завершенные задачи:", True)
+    work_todos(user["id"], file, "Оставшиеся задачи:", False)
     file.close()
 
 
-def work_todos(userId, file):
+def work_todos(user_Id, file, text_print, completed):
+    file.write('\n' + text_print + '\n')
     for todos_from_base in myTodosJson:
-        if userId == todos_from_base[userId] and todos_from_base["completed"] == "true":
+        if user_Id == todos_from_base["userId"] and todos_from_base["completed"] == completed:
             task = todos_from_base["title"]
-            if len(task) > 50:
-                task[0, 50]
-                file.write(task + "..." + '\n')
+            if len(task) >= 51:
+                file.write(task[0: 50] + "..." + '\n')
             else:
                 file.write(task + '\n')
+
 
 
 for user_from_base in myUserJson:
