b3f61629338449358ecbd6c15bd4e9c51a953171
GlynTwain <tri.dryga.new@gmail.com>
2020-03-17 18:57:19 +0300

    Основная часть готова, сырой код выполняет свою функцию полностью, но не обрабатываются все исключения и не выполняются некоторые условия

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ if not os.path.exists(folderName):
     os.mkdir(folderName)
 
 todos_json = requests.get("https://json.medrating.org/todos")
+myTodosJson = json.loads(todos_json.text)
 user_json = requests.get("https://json.medrating.org/users")
 myUserJson = json.loads(user_json.text)
 
@@ -31,31 +32,62 @@ def creation_date(path_to_file):
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
+
 def modification_date_for_rename(filename):
     t = os.path.getmtime(filename)
     return datetime.datetime.fromtimestamp(t).strftime("%d-%m-%YT %I-%M")
 
+
 def modification_date(filename):
     t = os.path.getmtime(filename)
     return datetime.datetime.fromtimestamp(t).strftime("%d.%m.%Y %I:%M")
+
 
 def create_file(user):
     """Метод создания файлов"""
     name_file = folderName + "/" + user["username"] + ".txt"
 
     if os.path.exists(name_file):
-        new_name = folderName + "/" + user["username"]+"_"+modification_date_for_rename(name_file)+ ".txt"
+        new_name = folderName + "/" + user["username"] + "_" + modification_date_for_rename(name_file) + ".txt"
         os.rename(name_file, new_name)
 
     if not os.path.exists(name_file):
-        filling(user,name_file)
+        filling(user, name_file)
 
-def filling(user,name_file):
 
+def filling(user, name_file):
     file = open(name_file, mode='w+', encoding='utf-8')
-    file.write(user["name"] + " <"+user["email"]+"> "+ modification_date(name_file) + '\n')
-    file.write(user["company"]["name"] + '\n'+'\n')
+    file.write(user["name"] + " <" + user["email"] + "> " + modification_date(name_file) + '\n')
+    file.write(user["company"]["name"] + '\n')
+    file.write('\n'+"Завершенные задачи:" + '\n')
+    for todos_from_base in myTodosJson:
+        if user["id"] == todos_from_base["userId"] and todos_from_base["completed"] == True:
+            task = todos_from_base["title"]
+            if len(task) >= 51:
+                file.write(task[0: 50] + "..." + '\n')
+            else:
+                file.write(task + '\n')
+    file.write('\n'+"Оставшиеся задачи:" + '\n')
+    for todos_from_base in myTodosJson:
+        if user["id"] == todos_from_base["userId"] and todos_from_base["completed"] == False:
+            task = todos_from_base["title"]
+            if len(task) >= 51:
+                file.write(task[0: 50] + "..." + '\n')
+            else:
+                file.write(task + '\n')
     file.close()
+
+
+def work_todos(userId, file):
+    for todos_from_base in myTodosJson:
+        if userId == todos_from_base[userId] and todos_from_base["completed"] == "true":
+            task = todos_from_base["title"]
+            if len(task) > 50:
+                task[0, 50]
+                file.write(task + "..." + '\n')
+            else:
+                file.write(task + '\n')
+
 
 for user_from_base in myUserJson:
     create_file(user_from_base)
